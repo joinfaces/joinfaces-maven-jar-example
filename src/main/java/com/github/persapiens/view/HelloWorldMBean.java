818710dd4aef3d8d4a9ee1b1e23e7fd9a29764db
Marcelo Romulo Fernandes <marcelo.fernandes@tre-rn.jus.br>
2016-05-12 09:13:44 -0300

    fix #2

---
+++ src/main/java/com/github/persapiens/view/HelloWorldMBean.java
@@ -0,0 +1,30 @@
+package com.github.persapiens.view;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class HelloWorldMBean {
+    
+    private String nickname;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    
+    public void buttonAction() {
+        addMessage("Welcome " + nickname + "!!!");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }    
+}
