beaac6b248a30cc6240711b5663b831e946e165d
Marcelo Romulo Fernandes <marcelo.fernandes@tre-rn.jus.br>
2016-05-12 09:12:13 -0300

    fix #3

---
+++ src/main/java/com/github/persapiens/JsfSpringBootStarterExampleApplication.java
@@ -0,0 +1,17 @@
+package com.github.persapiens;
+
+import com.github.persapiens.jsfboot.CdiScopeResolver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@ComponentScan(scopeResolver = CdiScopeResolver.class)
+@Configuration
+public class JsfSpringBootStarterExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfSpringBootStarterExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/persapiens/JsfSpringBootStarterExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.persapiens;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JsfSpringBootStarterExampleApplication.class)
+public class JsfSpringBootStarterExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
