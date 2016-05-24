a361d4a3e99b42952c23997d59c8ff38d2e993fb
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-05-24 19:41:12 -0300

    fix #9 change jsf libraries and servlet containers logo improve helloWorld.xhtml layout change test to integration test

--- src/test/java/com/github/persapiens/example/JsfSpringBootStarterExampleApplicationIT.java
+++ src/test/java/com/github/persapiens/example/JsfSpringBootStarterExampleApplicationIT.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = JsfSpringBootStarterExampleApplication.class)
-public class JsfSpringBootStarterExampleApplicationTests {
+public class JsfSpringBootStarterExampleApplicationIT {
 
 	@Test
 	public void contextLoads() {
