ee1cc11f89733ce9c74231456990a1b9b5bf812f
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-01 12:44:08 -0300

    fix #20 SecurityConfig.java created with two memory users: persapiens/123/ROLE_ADMIN and nyilmaz/qwe/ROLE_USER

---
+++ src/main/java/com/github/persapiens/example/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.github.persapiens.example;
+
+import java.util.Arrays;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter 
+{
+
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+      http
+          .userDetailsService(userDetailsService())
+          .formLogin()
+            .defaultSuccessUrl("/helloWorld.jsf").and()
+          .csrf()
+            .disable()
+          .authorizeRequests()
+            .anyRequest().authenticated();
+   }
+
+   @Override
+   public void configure(WebSecurity web) throws Exception {
+      web.ignoring().antMatchers("javax.faces.resources/*");
+   }
+
+   @Override
+   protected UserDetailsService userDetailsService() {
+      UserDetails user1 = new User("persapiens", "123", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN"));
+      UserDetails user2 = new User("nyilmaz", "qwe", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+      return new InMemoryUserDetailsManager(Arrays.asList(user1, user2));
+   }
+}
