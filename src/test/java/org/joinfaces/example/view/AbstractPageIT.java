31c37513699ee7c46866fc3a4d19e1580534ab21
Marcelo Romulo Fernandes <persapiens@gmail.com>
2022-05-21 20:58:17 -0300

    fix compilation

--- src/test/java/org/joinfaces/example/view/AbstractPageIT.java
+++ src/test/java/org/joinfaces/example/view/AbstractPageIT.java
@@ -28,7 +28,7 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.PageFactory;
 
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 /**
  * Abstract class to create utility methods to access WebClient and Page.
