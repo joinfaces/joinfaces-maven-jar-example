d0ebeec52f5bf522783c50f8b75f09284514e247
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-04-06 17:39:17 -0300

    remove unused import

--- src/main/java/org/joinfaces/example/view/JoinFacesStarterService.java
+++ src/main/java/org/joinfaces/example/view/JoinFacesStarterService.java
@@ -19,7 +19,6 @@ package org.joinfaces.example.view;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
