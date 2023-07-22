a00286bf813a9020f7c7ec686c4f5a8087194bd2
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-07-22 20:23:56 -0300

    compile fix

--- src/main/java/org/joinfaces/example/SecurityConfig.java
+++ src/main/java/org/joinfaces/example/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
